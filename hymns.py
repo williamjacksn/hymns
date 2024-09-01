3b3bfef612f69b26306f0f0caf68a58db92d60ba
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-08-31 19:16:10 -0500

    Create hymns.py

---
+++ hymns.py
@@ -0,0 +1,56 @@
+h = 'https://assets.churchofjesuschrist.org'
+
+hymn_data = [
+    {
+        'number': 1001,
+        'pdf_url': f'{h}/6e/4e/6e4ecd14ebb511ee863aeeeeac1eaa7887580260/come_thou_fount_of_every_blessing.pdf'
+    },
+    {
+        'number': 1002,
+        'pdf_url': f'{h}/70/a7/70a7ff54ebb511eeb4d5eeeeac1ea6754c802f3d/when_the_savior_comes_again.pdf'
+    },
+    {
+        'number': 1003,
+        'pdf_url': f'{h}/6f/d3/6fd34a32ebb511eea3b1eeeeac1e2cdd0ffc6784/it_is_well_with_my_soul.pdf'
+    },
+    {
+        'number': 1004,
+        'pdf_url': f'{h}/6f/cb/6fcb5af5ebb511ee903eeeeeac1e7de67f84090e/i_will_walk_with_jesus.pdf'
+    },
+    {
+        'number': 1005,
+        'pdf_url': f'{h}/6f/00/6f001bb4ebb511eea424eeeeac1e211e768ee551/his_eye_is_on_the_sparrow.pdf'
+    },
+    {
+        'number': 1006,
+        'pdf_url': f'{h}/70/39/703937f5ebb511ee888feeeeac1e36706b2e5f48/think_a_sacred_song.pdf'
+    },
+    {
+        'number': 1007,
+        'pdf_url': f'{h}/6d/f3/6df3dbd5ebb511eea9a7eeeeac1eca3ca4ff6c01/as_bread_is_broken.pdf'
+    },
+    {
+        'number': 1008,
+        'pdf_url': f'{h}/6e/06/6e062b55ebb511eeb343eeeeac1ef072e68df91f/bread_of_life_living_water.pdf'
+    },
+    {
+        'number': 1009,
+        'pdf_url': f'{h}/6e/79/6e795f87ebb511eebd27eeeeac1ef760eeedab62/gethsemane.pdf'
+    },
+    {
+        'number': 1201,
+        'pdf_url': f'{h}/6e/9f/6e9fac3debb511eea036eeeeac1ec84f90cdba38/hail_the_day_that_sees_him_rise.pdf'
+    },
+    {
+        'number': 1202,
+        'pdf_url': f'{h}/6e/d8/6ed86f77ebb511eeb141eeeeac1eae03688eebd1/he_is_born_the_divine_christ_child.pdf'
+    },
+    {
+        'number': 1203,
+        'pdf_url': f'{h}/70/73/707381d2ebb511eeb518eeeeac1e3b993d94e337/what_child_is_this.pdf'
+    },
+    {
+        'number': 1204,
+        'pdf_url': f'{h}/6f/fb/6ffbe0d8ebb511eeb958eeeeac1e769f17b3574f/star_bright.pdf'
+    }
+]
