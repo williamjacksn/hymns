f4fe48bf07007dd31a47ca6d000f266dcc550914
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-02-18 07:03:48 -0600

    Update gen-all.py to use choice lists defined in hymns module

--- gen-all.py
+++ gen-all.py
@@ -2,15 +2,6 @@ import hymns
 import itertools
 
 if __name__ == '__main__':
-    lang_choices = size_choices = []
-
-    parser = hymns.arg_parser()
-    for action in parser._actions:
-        if action.dest == 'lang':
-            lang_choices = action.choices
-        elif action.dest == 'size':
-            size_choices = action.choices
-
-    for (lang, size) in itertools.product(lang_choices, size_choices):
+    for (lang, size) in itertools.product(hymns.lang_choices, hymns.size_choices):
         print(f'Generating document for lang:{lang} and size:{size}')
         hymns.build_pdf(lang, size)
