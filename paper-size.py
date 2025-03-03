9bc3edd3bf6ab6d52d7e5141de855b3a25e7e9b7
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-03-03 09:46:51 -0600

    Add a tool to show paper size of an existing PDF

---
+++ paper-size.py
@@ -0,0 +1,10 @@
+import argparse
+import pymupdf
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file')
+args = parser.parse_args()
+
+doc = pymupdf.open(args.file)
+page = doc[0]
+print(page.rect.width, page.rect.height)
